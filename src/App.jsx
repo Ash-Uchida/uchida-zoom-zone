820cc0bf35f41734b543562a8bc810236ebfc0a2
Ash-Uchida <ash.uchida0w0@gmail.com>
2025-12-02 17:02:34 -0700

    fingers crossed

--- src/App.jsx
+++ src/App.jsx
@@ -139,7 +139,7 @@ export default function App() {
           />
 
           <label>
-            Meeting Time (MST):
+            Meeting Time:
             {loadingSlots ? (
               <div>Loading times...</div>
             ) : availableSlots.length === 0 ? (
